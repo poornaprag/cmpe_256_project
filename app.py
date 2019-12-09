f555d9c1739b0b5f291646d64feb0bd711cf3fbd
Poorna <poornaprag@gmail.com>
2019-12-08 22:36:18 -0800

    made some changes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, url_for, request
-# import tweepy as tw
+import tweepy as tw
 import json
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from sklearn.feature_extraction.text import (
+    CountVectorizer,
+    TfidfVectorizer,
+    HashingVectorizer,
+)
 import pickle
 import joblib
 import pandas as pd
@@ -12,10 +16,25 @@ from nltk.stem import WordNetLemmatizer
 
 app = Flask(__name__)
 
-@app.route("/")
+# Reading credentials file for Twitter
+data_file = open("credentials.json", "r")
+data = json.load(data_file)
+
+# Getting all keys for Twitter
+consumer_key = data["consumer_key"]
+consumer_secret = data["consumer_secret"]
+access_token = data["access_token"]
+access_token_secret = data["access_token_secret"]
+
+# Accessing Twitter API
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+
 @app.route("/how")
 def how():
-    return render_template('how.html', title='How It Works')
+    return render_template("how.html", title="How It Works")
 
 
 # def tokenize(sub):
@@ -29,48 +48,53 @@ def how():
 
 #     return words
 
+
 def svm(sample_test):
-    vect = joblib.load('vect1.pkl')
-    loaded_model = joblib.load('sv.pkl')
+    vect = joblib.load("vect1.pkl")
+    loaded_model = joblib.load("sv.pkl")
     sample_test_dtm = vect.transform([sample_test])
     result = loaded_model.predict(sample_test_dtm)
     return result
+
 
 def knn(sample_test):
-    vect = joblib.load('vect1.pkl')
-    loaded_model = joblib.load('lr.pkl')
+    vect = joblib.load("vect1.pkl")
+    loaded_model = joblib.load("lr.pkl")
     sample_test_dtm = vect.transform([sample_test])
     result = loaded_model.predict(sample_test_dtm)
     return result
+
 
 def naive_bayes(sample_test):
-    vect = joblib.load('vect1.pkl')
-    loaded_model = joblib.load('nb.pkl')
+    vect = joblib.load("vect1.pkl")
+    loaded_model = joblib.load("nb.pkl")
 
     sample_test_dtm = vect.transform([sample_test])
     result = loaded_model.predict(sample_test_dtm)
     pdx = pd.DataFrame(result)
-    what = pdx[0].value_counts().to_json(orient='records')
+    what = pdx[0].value_counts().to_json(orient="records")
 
     return result
+
 
 def nb2(sample_test):
-    vect = joblib.load('vect2.pkl')
-    loaded_model = joblib.load('nb2.pkl')
+    vect = joblib.load("vect2.pkl")
+    loaded_model = joblib.load("nb2.pkl")
 
     sample_test_dtm = vect.transform([sample_test])
     result = loaded_model.predict(sample_test_dtm)
     pdx = pd.DataFrame(result)
-    what = pdx[0].value_counts().to_json(orient='records')
+    what = pdx[0].value_counts().to_json(orient="records")
 
     return result
+
 
 def get_sample_data():
     d1 = pd.read_csv("comments_1.csv")
     d2 = pd.read_csv("comments_2.csv")
     d3 = d1.append(d2)
-    df=pd.DataFrame(d3)
-    
+    df = pd.DataFrame(d3)
+
     from sklearn.model_selection import train_test_split
 
     X = df.headline
@@ -80,9 +104,9 @@ def get_sample_data():
 
     from sklearn.feature_extraction.text import CountVectorizer
 
-    vect1 = CountVectorizer(ngram_range=(1,1))
+    vect1 = CountVectorizer(ngram_range=(1, 1))
 
-    vect2 = CountVectorizer(ngram_range=(2,2))
+    vect2 = CountVectorizer(ngram_range=(2, 2))
 
     X_train_vect = vect1.fit_transform(X_train)
 
@@ -94,26 +118,31 @@ def get_sample_data():
 
     X_train_res, y_train_res = sm.fit_sample(X_train_vect, y_train)
     import numpy as np
+
     unique, counts = np.unique(y_train_res, return_counts=True)
-    
+
     result = list(zip(unique, counts))
     negative = None
     positive = None
-   
+
     if result[0][0] == -1:
         negative = result[0][1]
         positive = result[1][1]
     else:
         negative = result[1][1]
         positive = result[0][1]
-    final_res = [{"y":int(positive),"label":"Postive"},{"y":int(negative),"label":"Negative"}]
+    final_res = [
+        {"y": int(positive), "label": "Postive"},
+        {"y": int(negative), "label": "Negative"},
+    ]
 
     return final_res
 
-@app.route('/result', methods=['POST'])
+
+@app.route("/result", methods=["POST"])
 def result():
-    if request.method == 'POST':
-        sentence = request.form['text']
+    if request.method == "POST":
+        sentence = request.form["text"]
         nb_result = naive_bayes(sentence)
         knn_result = knn(sentence)
         svm_result = svm(sentence)
@@ -124,38 +153,74 @@ def result():
         prediction.append(knn_result)
         prediction.append(svm_result)
         prediction.append(nb2_result)
-        
+
         return render_template("result.html", prediction=prediction, sentence=sentence)
 
 
 @app.route("/data")
 def data():
-    return render_template('data.html', title='Data We Use')
+    # if username == '':
+    #     username = 'realDonaldTrump'
+    users_id = ["1425014252"]
+    usernames = []
+    for id in users_id:
+        usernames.append(api.get_user(id).screen_name)
+    usernames.append("realDonaldTrump")
+    return render_template("data.html", title="Data We Use", users=usernames)
+
+
+@app.route("/data/<string:username>")
+def data_user(username):
+    def get_tweets(username):
+        tweets = api.user_timeline(screen_name=username)
+        return [
+            {
+                "tweet": t.text,
+                "created_at": t.created_at,
+                "username": username,
+                "headshot_url": t.user.profile_image_url,
+            }
+            for t in tweets
+        ]
+
+    return render_template(
+        "data.html", title="Data We Use", tweets=get_tweets(username)
+    )
+
+
+@app.route("/")
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
 
 @app.route("/visualize")
 def vis():
     df = pd.read_csv("comments_1.csv")
     df1 = pd.read_csv("comments_2.csv")
     df = df.append(df1)
-    df.columns=['x','y']
+    df.columns = ["x", "y"]
 
-    data = df['y'].value_counts()
+    data = df["y"].value_counts()
 
-    x=pd.DataFrame(data)
-    x['label']=['0','-1']
-    
-    sent = {'0' : 'Postive', '-1': 'Negative'} 
-        
-    x.label = [sent[item] for item in x.label] 
-    
-    result = x.to_json(orient='records')
+    x = pd.DataFrame(data)
+    x["label"] = ["0", "-1"]
+
+    sent = {"0": "Postive", "-1": "Negative"}
+
+    x.label = [sent[item] for item in x.label]
+
+    result = x.to_json(orient="records")
     # return result
-    import json 
+    import json
+
     sample_data = json.dumps(get_sample_data())
-    print (result)
-    print (sample_data)
-    return render_template('visual.html', title='Visualize',result=result, sample_data=sample_data)
+    print(result)
+    print(sample_data)
+    return render_template(
+        "visual.html", title="Visualize", result=result, sample_data=sample_data
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
